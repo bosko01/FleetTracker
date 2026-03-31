636db286bebe11fc881d17ff2d9a930b8cdd4167
bosko01 <120031637+bosko01@users.noreply.github.com>
2026-03-31 16:01:52 +0200

    Fix API CORS policy for local frontend

--- src/FleetTracker.API/Program.cs
+++ src/FleetTracker.API/Program.cs
@@ -10,6 +10,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Frontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
 builder.Services
     .AddApplicationServices()
@@ -35,6 +44,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("Frontend");
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
