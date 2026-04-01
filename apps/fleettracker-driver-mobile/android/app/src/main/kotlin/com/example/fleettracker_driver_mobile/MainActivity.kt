a41c6798f46bdaa485caeed13c5b09a97c20de1b
bosko01 <vujanovicbosko01@gmail.com>
2026-04-01 17:02:10 +0200

    initialized flutter app

---
+++ apps/fleettracker-driver-mobile/android/app/src/main/kotlin/com/example/fleettracker_driver_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fleettracker_driver_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ src/FleetTracker.Infrastructure/Migrations/20260331154824_AddInitialMileageToVehicle.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FleetTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInitialMileageToVehicle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
